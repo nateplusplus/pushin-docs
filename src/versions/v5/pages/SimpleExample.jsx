43def6bcd83701499925057a07914441ddddd126
Nathan Blair <nateblairfilm@gmail.com>
2023-04-08 11:58:00 -0500

    #18: Add support for versioned docs

---
+++ src/versions/v5/pages/SimpleExample.jsx
@@ -0,0 +1,35 @@
+import PageLayout from "../components/PageLayout";
+import { useLayoutEffect, useRef } from "react";
+
+import { PushIn } from "pushin";
+import 'pushin/dist/pushin.css';
+
+export default function SimpleExample() {
+  const pushInContainer = useRef();
+
+  useLayoutEffect(() => {
+      const pushIn = new PushIn(pushInContainer.current);
+      pushIn.start();
+
+      return () => pushIn.destroy();
+  });
+
+  return (
+    <PageLayout id="page-simple-example">
+      <div ref={pushInContainer} className="pushin">
+        <div className="pushin-layer">
+          This example demonstrates the simplest, zero-configuration setup for pushin.js.
+        </div>
+        <div className="pushin-layer">
+          These layers are added to the HTML without any parameters and minimal CSS.
+        </div>
+        <div className="pushin-layer">
+          Pushin.js has configured the timing of this effect automatically to adjust for the number of layers.
+        </div>
+        <div className="pushin-layer">
+          The scroll length increases to accommodate all layers in an evenly-spaced animation.
+        </div>
+      </div>
+    </PageLayout>
+  );
+}
